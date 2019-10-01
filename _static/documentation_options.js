3c22c26d7e86bcd2789a42f19abe4d2d6087d70c
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-10-01 05:33:07 +0000

    Update docs after building Travis build 5 of betsig/borf
    The docs were built from the branch 'master' against the commit
    393b89cdcfe65c4678e4b8e0300ebfc02f522893.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/betsig/borf/jobs/591846072.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post37+g9b7c11e',
+    VERSION: '0.post38+g393b89c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
