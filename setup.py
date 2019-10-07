e363040257d1c370da3c54a14e59abb7ae59c295
betsig <beth.signal@gmail.com>
2019-10-08 09:48:45 +1100

    added long_description_content_type to pass twine without warnings

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT",
+    long_description_content_type="text/x-rst",
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Natural Language :: English',
